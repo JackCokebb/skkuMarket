93dda4b44dc136850da60b0f798bf81132a89cab
JackCokebb <reo2726@gmail.com>
2023-01-09 19:42:48 +0900

    feat : project init

---
+++ src/main/java/edu/swcoaching/skkumarket/SkkuMarketApplication.java
@@ -0,0 +1,13 @@
+package edu.swcoaching.skkumarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkkuMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkkuMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/swcoaching/skkumarket/SkkuMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.swcoaching.skkumarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkkuMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
